cfe7289e06dc9be6322b9ecc84c54c830e715211
LKacik <cep6@poczta.fm>
2022-09-05 20:43:03 +0200

    Created class CustomUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    STATUS = (
+        ('owner', 'owner'),
+        ('observer', 'observer'),
+        ('moderator', 'moderator')
+    )
+
+    email = models.EmailField(unique=True)
+    status = models.CharField(max_length=128, choices=STATUS, default='observer')
+    description = models.TextField('Description', max_length=512, default='', blank=True)
+
+    def __str__(self):
+        return self.username

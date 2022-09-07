63750243e947f961b43d18fabbf64ce46d887b94
LKacik <cep6@poczta.fm>
2022-09-07 22:03:01 +0200

    Added path -> add, remove, edit, search

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,9 @@ from warehouse import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main)
+    path('', views.main, name='main'),
+    path('add', views.add, name='add'),
+    path('remove', views.remove, name='remove'),
+    path('edit', views.edit, name='edit'),
+    path('search', views.search, name='search'),
 ]

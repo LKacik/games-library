8bd866950b01f3f9626eac9249a9f5c0c26973ae
LKacik <cep6@poczta.fm>
2022-09-07 22:09:09 +0200

    Created def add, remove, edit, search

--- warehouse/views.py
+++ warehouse/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main.html')
+
+
+def add(request):
+    return render(request, 'add.html')
+
+
+def remove(request):
+    return render(request, 'remove.html')
+
+
+def edit(request):
+    return render(request, 'edit.html')
+
+
+def search(request):
+    return render(request, 'search.html')

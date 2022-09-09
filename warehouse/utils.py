c1604ddc70088e23def2480b04c76947ae329713
LKacik <cep6@poczta.fm>
2022-09-09 22:02:02 +0200

    Removed api key

--- warehouse/utils.py
+++ warehouse/utils.py
@@ -3,12 +3,12 @@ import requests
 
 
 
-url = "https://api.rawg.io/api/games/cannon-fodder?key=b55a581e99e8409aba9f9aee43603202"
-# url = "https://api.rawg.io/api/games?key=b55a581e99e8409aba9f9aee43603202&search=cannon-fodder&platforms=166"
-# url = "https://api.rawg.io/api/games?key=b55a581e99e8409aba9f9aee43603202"
+url = "https://api.rawg.io/api/games/cannon-fodder?key="
+# url = "https://api.rawg.io/api/games?key=&search=cannon-fodder&platforms=166"
+# url = "https://api.rawg.io/api/games?key="
 
 headers = {
-	"X-RapidAPI-Key": "17998f3b96msh01d58ff55e3cd95p12d75ejsnf13360bc660d",
+
 	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"
 }
 

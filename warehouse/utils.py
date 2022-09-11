4a31f5f493a5dc94c73205c3d2c48bd4f1f7e51c
LKacik <cep6@poczta.fm>
2022-09-11 23:23:17 +0200

    Created new response, downloaded db fields

--- warehouse/utils.py
+++ warehouse/utils.py
@@ -1,18 +1,46 @@
+import json
 from pprint import pprint
 import requests
 
 
 
-url = "https://api.rawg.io/api/games/cannon-fodder?key="
-# url = "https://api.rawg.io/api/games?key=&search=cannon-fodder&platforms=166"
-# url = "https://api.rawg.io/api/games?key="
+# url = "https://api.rawg.io/api/games/cannon-fodder?key="
+# # url = "https://api.rawg.io/api/games?key=&search=cannon-fodder&platforms=166"
+# # url = "https://api.rawg.io/api/games?key="
+#
+# headers = {
+#
+# 	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"
+# }
+#
+# response = requests.request("GET", url, headers=headers)
+#
+# pprint(response.text)
+# print(type(response))
 
-headers = {
+response = requests.get("https://api.rawg.io/api/games/cannon-fodder?key=b55a581e99e8409aba9f9aee43603202&platforms=10&page=1")
 
-	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"
-}
+if response.status_code != requests.codes.ok:
+	print('Error')
+else:
+	# print(json.dumps(response.json(), indent=4))
+	json_string = json.dumps(response.json(), indent=4)
+	data = json.loads(json_string)
+	pprint(data)
 
-response = requests.request("GET", url, headers=headers)
+	description = data['description']
+	name = data['name']
+	slug = data['slug']
+	developers = data['developers'][0]['name']
+	platform = [component['platform']['name'] for component in data['platforms']]
+	released = data['released']
 
-pprint(response.text)
-print(type(response))
+	print(description)
+	print(name)
+	print(slug)
+	print(developers)
+	print(platform)
+	print(released)
+
+# for dat in data['platforms']:
+# 	print(dat['platform']['name'])

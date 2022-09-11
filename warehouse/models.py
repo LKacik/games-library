833bab36678da40026dac1e367dfff37ba72a431
LKacik <cep6@poczta.fm>
2022-09-11 21:40:22 +0200

    Created model Games

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Games(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField()
+    description = models.TextField()
+    platform = models.CharField(max_length=192)
+    image = models.ImageField()
+    released = models.DateField()
+
+

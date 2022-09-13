0f8bb478687b244f96a9d04d5a661fe190e1f566
LKacik <cep6@poczta.fm>
2022-09-13 22:17:08 +0200

    Sending a query to Api, downloading and saving in db

--- warehouse/utils.py
+++ warehouse/utils.py
@@ -1,46 +1,1 @@
-import json
-from pprint import pprint
-import requests
-
-
-
-# url = "https://api.rawg.io/api/games/cannon-fodder?key="
-# # url = "https://api.rawg.io/api/games?key=&search=cannon-fodder&platforms=166"
-# # url = "https://api.rawg.io/api/games?key="
-#
-# headers = {
-#
-# 	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"
-# }
-#
-# response = requests.request("GET", url, headers=headers)
-#
-# pprint(response.text)
-# print(type(response))
-
-response = requests.get("https://api.rawg.io/api/games/cannon-fodder?key=b55a581e99e8409aba9f9aee43603202&platforms=10&page=1")
-
-if response.status_code != requests.codes.ok:
-	print('Error')
-else:
-	# print(json.dumps(response.json(), indent=4))
-	json_string = json.dumps(response.json(), indent=4)
-	data = json.loads(json_string)
-	pprint(data)
-
-	description = data['description']
-	name = data['name']
-	slug = data['slug']
-	developers = data['developers'][0]['name']
-	platform = [component['platform']['name'] for component in data['platforms']]
-	released = data['released']
-
-	print(description)
-	print(name)
-	print(slug)
-	print(developers)
-	print(platform)
-	print(released)
-
-# for dat in data['platforms']:
-# 	print(dat['platform']['name'])
+import jsonfrom pprint import pprintimport requestsimport osos.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'import djangodjango.setup()from warehouse.models import Games# url = "https://api.rawg.io/api/games/cannon-fodder?key="# # url = "https://api.rawg.io/api/games?key=&search=cannon-fodder&platforms=166"# # url = "https://api.rawg.io/api/games?key="## headers = {## 	"X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"# }## response = requests.request("GET", url, headers=headers)## pprint(response.text)# print(type(response))response = requests.get("https://api.rawg.io/api/games/cannon-fodder?key=&platforms=10&page=1")if response.status_code != requests.codes.ok:	print('Error')else:	# print(json.dumps(response.json(), indent=4))	json_string = json.dumps(response.json(), indent=4)	data = json.loads(json_string)	pprint(data)	description = data['description']	name = data['name']	slug = data['slug']	developers = data['developers'][0]['name']	platform = [component['platform']['name'] for component in data['platforms']]	released = data['released']	print(description)	print(name)	print(slug)	print(developers)	print(platform)	print(released)# for dat in data['platforms']:# 	print(dat['platform']['name'])	add_games = Games()	add_games.name = name	add_games.released = released	add_games.slug = slug	add_games.description = description	add_games.platform = platform	add_games.developers = developers	add_games.save()
